d1d1acffe0b5eddd9a3a0703983cbecbbc424bf7
Lin ZHANG <lzhangbj@connect.ust.hk>
2020-05-01 23:43:03 +0800

    add app

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def make_prediction():
 
 if __name__ == '__main__':
 	# start api
-	app.run(host='127.0.0.1', port=5000, debug=False)
+	# app.run(host='127.0.0.1', port=5000, debug=False)
+	app.run(port=5000, debug=False)
